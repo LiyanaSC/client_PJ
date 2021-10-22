69b04e1a46342f10d461b917f6086532b2fd7043
OPBS Liyana <liyana.perso@gmail.com>
2021-10-22 15:38:15 -0400

    first step

---
+++ pj_app/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <img src="./assets/agenda.svg" alt="icone d'accès à l'agenda">
+      <img src="./assets/image.svg" alt="icone d'accès à la galerie d'image">
+      <router-link to="/"><img src="./assets/home.svg" alt="icone d'accès à l'accueil"></router-link>
+
+
+      <img src="./assets/contact.svg" alt="icone d'accès au formulaire de contact">
+      <router-link to="/about"><img src="./assets/information.svg" alt="icone d'accès à la partie à propos"></router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0%;
+}
+
+#nav {
+  padding: 30px;
+  position: fixed;
+  bottom: 0px;
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+  align-items: center;
+  background-color: white;
+  border-top: 1px gray solid;
+  margin: 0%;
+  max-height: 100px;
+  z-index: 10;
+
+ 
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+  img{
+    max-height: 50px;
+  }
+}
+</style>
---
+++ pj_app/src/views/Home.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="home">
+    
+    <img class="beach_pic" alt="Photo de la plage de Saint-Anne" src="../assets/plage.jpeg">
+    <div class="logo"><h1 class="two_rem">Chez <br/> Patricia et Joel</h1></div>
+    <div class="text">
+        <h2 class="two_rem">Maison en location saisonnière sur une île de rêve  </h2>
+        <p>Découvrez la maison de vacance idéale pour votre séjour en martinique</p>
+    </div>
+
+    <div class="go_to_gallery">
+        <p>Découvrez la maison de vacance idéale pour votre séjour en martinique</p>
+        <button class="btn_style">Voir la galerie</button>
+    </div>
+
+    <p>La maison est située au sud de la Martinique</p>
+    <p>Au plein coeur du bourg de Sainte-Anne, profitez de toutes les commodité et de notre belle plage à 5 min de marche à pied🚶🏻‍♀️</p>
+
+    <div class="go_to_contact">
+      <div class="caption">
+        <p>Faite le pleins de souvenirs en réservant une maison qui vous plonge au coeur de la Martinique</p>
+        <button class="btn_style">Je veux réserver</button>
+      </div>
+    </div>
+    <div>
+       <p>Venez profiter de l'air Martiniquais toute l'année!</p>
+       <button class="btn_style btn_color_two"> Je vérifie les dates de disponibilité!</button>
+
+    </div>
+   
+
+
+  
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
+<style lang="scss" scoped>
+.home{
+  margin: 0%;
+  width: 100%;
+  margin-bottom: 200px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.beach_pic{
+height: 200px;
+overflow: hidden;
+width: 100%;
+object-fit: cover;
+margin: 0%;
+object-position: center;  
+}
+.logo{
+ 
+  position: absolute;
+  top: 100px;
+  border-radius: 50%;
+ background-color: skyblue;
+ height: 200px;
+ width: 200px;
+ border:5px #fff solid;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ font-family: 'Dancing Script', cursive;
+
+color: #fff;
+}
+.text{
+  margin-top: 100px;
+  font-weight: bold;
+}
+.two_rem{
+   font-size: 2rem;
+}
+
+.go_to_gallery{
+  position: relative;
+  margin: 0%;
+  width: 100%;
+  height: 300px;
+  background-image: url("../assets/photos.jpg");
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+   color: #fff;
+  font-weight: bold;
+  text-shadow: 1px 2px 2px black;
+  font-size: 1.5rem;
+
+}
+
+
+.go_to_contact{
+  position: relative;
+  margin: 0%;
+  width: 100%;
+  height: 300px;
+  background-image: url("../assets/souvenirs.jpg");
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  align-items: center;
+
+}
+
+.caption{
+
+  top:30px;
+  left:0px;
+  width: 47%;
+  color: #fff;
+  font-weight: bold;
+  text-shadow: 1px 2px 2px black;
+}
+
+.btn_style{
+  border-radius: 25px 25px 25px 25px ;
+  height: 50px;
+  background-color: palevioletred;
+  color: #fff;
+}
+.btn_color_two{
+background-color:skyblue ;
+}
+</style>
